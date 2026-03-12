efa960f044b87f68c2fecbff509396ce6743a68d
Richard Kindler <223166946+RickCreator87@users.noreply.github.com>
2026-03-12 12:39:34 -0600

    Create Codecerl.py
    Signed-off-by: Richard Kindler <223166946+RickCreator87@users.noreply.github.com>

---
+++ Codecerl.py
@@ -0,0 +1,52 @@
+"""Run this model in Python
+
+
+
+> pip install mistralai>=1.0.0
+
+"""
+
+import os
+
+from mistralai import Mistral, UserMessage, SystemMessage
+
+
+
+# To authenticate with the model you will need to generate a personal access token (PAT) in your GitHub settings. 
+
+# Create your PAT token by following instructions here: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+
+client = Mistral(
+
+    api_key=os.environ["GITHUB_TOKEN"],
+
+    server_url="https://models.github.ai/inference"
+
+)
+
+
+
+response = client.chat(
+
+    model="mistral-ai/Codestral-2501",
+
+    messages=[
+
+        SystemMessage("You are a helpful assistant."),
+
+        UserMessage("What is the capital of France?"),
+
+    ],
+
+    temperature=1.0,
+
+    max_tokens=1000,
+
+    top_p=1.0
+
+)
+
+
+
+print(response.choices[0].message.content)
+
